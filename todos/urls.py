cf53725941c37c9ac4afee48c16c19588268f712
alirazabhayani <alirazabhayani@gmail.com>
2017-12-15 22:44:57 +0500

    todolist6 Step 6

---
+++ todos/urls.py
@@ -0,0 +1,10 @@
+
+from django.conf.urls import include, url
+
+from . import views
+
+urlpatterns = [
+    url(r'^test/$', views.test, name='test'),
+    url(r'^$', views.index, name='index'),
+    url(r'^details/(?P<id>\w{0,50})/$', views.details),
+]
